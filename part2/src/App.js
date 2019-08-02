0db7ae910ebd4886abf9b50645e958e56a80256c
Randall <Eric.Randall@amtrak.com>
2019-08-02 17:39:47 -0400

    Part 2c
    Just the examples

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes) 
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     'a new note...'
   )
   const [showAll, setShowAll] = useState(true)
+  
+  useEffect(() => {
+	  console.log('effect')
+	  axios
+		.get('http://localhost:3001/notes')
+		.then(response => {
+		  console.log('promise fulfilled')
+		  setNotes(response.data)
+    })
+  }, []) //empty array means useEffect only runs once on render
+  console.log('render', notes.length, 'notes')
+  
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important === true)
