92d2636d1a04d684b9d16c69295823ba9414ca16
retropean <eric.randalls@gmail.com>
2019-08-08 10:17:25 -0400

    Added temperature functionality
    From openweathermap.api. Used api code in components folder that is on .gtiignore list.

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-//import Name from './components/Name'
+import apikey from './components/api'
 
 function App() {
   const [ countries, setCountries] = useState([])
@@ -12,11 +12,11 @@ function App() {
 		.get('https://restcountries.eu/rest/v2/all')
 		.then(response => {
 		  setCountries(response.data)
-		  console.log('data pulled')
+		  //console.log('data pulled')
 		})
   }, [])
-  console.log('render', countries.length, 'countries')
-  console.log(countries)
+  //console.log('render', countries.length, 'countries')
+  //console.log(countries)
   
   const handleSearch = (event) => {
 	setSearchQuery(event.target.value)
@@ -70,21 +70,37 @@ function App() {
   )
 }
 
-
-const CountryDetail = ({ country }) => {
-  const [ countryweather, setCountryWeather] = useState([])
+const CountryWeather = ({ country }) => {
+  const [ countryWeather, setCountryWeather] = useState([])
   useEffect(() => {
 		  axios
-			.get('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=6c7b12680555fa5800396f77568c307f')
+			.get('http://api.openweathermap.org/data/2.5/weather?q='+country.capital+'&APPID='+apikey)
 			.then(response => {
-			  console.log(response.data)
-			  console.log('data pulled')
+			  setCountryWeather(response.data)
 			})
   }, [])
-  //console.log(countryWeather)
+  console.log(countryWeather)
+  if(countryWeather.length === 0)
+  {
+	return ( <div> </div> )
+  }
+  else
+  {
+    return (
+		<div>
+			<div><b>temperature:</b> {countryWeather.main.temp-273.15} celcius</div>
+			<div><b>wind:</b> {countryWeather.wind.speed} kph going {countryWeather.wind.deg}</div>
+		</div>
+    )
+  }
+}
+
+
+const CountryDetail = ({ country }) => {
   const countryLang = country.languages.map(language =>
-	<li>{language.name}</li>
+	<li key={language.name}>{language.name}</li>
   )
+  
   return (
     <div>
 		<h1>{country.name}</h1>
@@ -93,7 +109,10 @@ const CountryDetail = ({ country }) => {
 		<h2>languages</h2>
 		<div>{countryLang}</div>
 		<img src={country.flag} alt="Country Flag" width="200px"/>
-		
+		<CountryWeather
+		  key={country.name}
+		  country={country}
+		/>
 	</div>
   )
 }
