e362d708d0bef460f39e6c0465dc9aca4b31fe3f
retropean <eric.randalls@gmail.com>
2019-08-24 17:24:52 -0400

    3c pt2 example

--- part3/index.js
+++ part3/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Note = require('./models/note')
 
 app.use(express.static('build'))
 app.use(cors())
@@ -40,19 +41,9 @@ let notes = [
 ]
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  /*
-  const note = notes.find(note => {
-    console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-    return note.id === id
+  Note.findById(request.params.id).then(note => {
+    response.json(note.toJSON())
   })
-  */
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -71,32 +62,31 @@ const generateId = () => {
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
-  //return 400 bad request if content missing
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote.toJSON())
+  })
 })
 
 app.get('/api/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes.map(note => note.toJSON()))
+  });
+});
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
---
+++ part3/mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+var constants = require('./db_pass')
+
+if ( process.argv.length<3 ) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = constants.pass
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+/*
+const note = new Note({
+  content: 'Third note is third',
+  date: new Date(),
+  important: false,
+})
+
+note.save().then(response => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
+
+Note.find({important: true}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
